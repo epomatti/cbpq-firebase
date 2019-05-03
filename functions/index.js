d67ff24bcdd65d98dafa9be70fe10750d2519151
Evandro Pomatti <tecbmepi@TECLOGICA.COM.BR>
2019-05-03 11:13:12 -0300

    licenca api

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,14 @@ const express = require('express')
 
 const app = express()
 
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`)
+app.get('/licenca', (request, response) => {
+  let licenca
+  if(request.body.cbpq) {
+    licenca = 'cbpq'
+  } else if(request.body.cpf) {
+    licenca = 'cpf'
+  }
+  response.send(licenca)
 })
 
 exports.app = functions.https.onRequest(app)
