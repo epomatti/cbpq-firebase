8862ca6b569570bdfc504747cf74c784f544a9a6
Evandro Pomatti <evandro.pomatti@gmail.com>
2019-05-06 21:54:18 -0300

    validate

--- functions/test/validate.js
+++ functions/test/validate.js
@@ -1,19 +1,34 @@
+/* 
+* To run this program use:
+* node validate.js <start> <end> <threads>
+* 
+* Exemple: node validate.js 0 99 3
+*/
+
 const axios = require('axios')
 const cq = require('concurrent-queue')
+
+console.log(process.argv)
+
+const start = Number(process.argv[2])
+const end = Number(process.argv[3])
+const threads = Number(process.argv[4])
 
 const client = axios.create({
   baseURL: 'http://localhost:5001/cbpq-7e4c6/us-central1/app',
 })
 
-const queue = cq().limit({ concurrency: 1 }).process(task => task())
+const queue = cq().limit({ concurrency: threads }).process(task => task())
+
+let errors = 0
 
 const call = async (cbpq) => {
-  const response = await client.get(`/license/cbpq/${cbpq}`)
-  await response.data
+  await client.get(`/license/cbpq/${cbpq}`)
+    .catch(() => errors++);
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = start; i <= end; i++) {
   queue(() => call(i))
 }
 
-queue.drained(() => console.log('Finished'))
+queue.drained(() => console.log(`Finished with ${errors} errors`))
